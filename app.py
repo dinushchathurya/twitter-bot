adada8e0402399bf112854166ce6da4040b4bbf2
dinushchathurya <dinushchathurya21@gmail.com>
2019-08-28 22:13:13 +0530

    Create tweet like function

--- app.py
+++ app.py
@@ -18,6 +18,25 @@ class TwitterBot:
         bot.get('https://twitter.com/')
         #Pause your app for 3 seconds
         time.sleep(3)
+        email = bot.find_element_by_class_name ('email-input')
+        password = bot.find_element_by_class_name('session [password]')
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        #Auto click on the login button
+        password.send_keys(keys.RETURN)
+        time.sleep(3)
+    
+    #Create function to like all the tweets which include specified hashtag
+    def like_tweet(self,hashtag):
+        bot = self.bot
+        bot.get('https://twitter.com/search?q='+hashtag+'&src=typd')
+        time.sleep(3)
+    #Create instance of TwitterBot class
+    ed = TwitterBot('your email' , 'your password')
+    #Call the login function
+    ed.login()
 
 
 
